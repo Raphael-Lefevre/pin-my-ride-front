558d3f22024b219bf1c2d2abaebe70c156baa8b1
Laura Glutron <lauraglutron@MacBook-Pro.lan>
2021-01-13 16:56:55 +0100

    created first part of the squeletton

---
+++ src/components/Ride.jsx
@@ -0,0 +1,13 @@
+import { Container, Row } from 'reactstrap';
+
+function Ride() {
+  return (
+    <Container>
+      <Row className="d-flex justify-content-center">
+        <h1>Ride</h1>
+      </Row>
+    </Container>
+  );
+}
+
+export default Ride;
