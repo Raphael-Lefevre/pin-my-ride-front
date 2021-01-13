39442ff99df27149e10d1402e3b4ebefcf5b4413
Raphael Lefevre <raph.lefevre@gmail.com>
2021-01-13 19:41:25 +0100

    Deleted unnecessary comments

--- src/components/Ride.jsx
+++ src/components/Ride.jsx
@@ -59,17 +59,3 @@ Markers.propTypes = {
 };
 
 export default Map;
-
-// import { Container, Row } from 'reactstrap';
-
-// function Ride() {
-//   return (
-//     <Container>
-//       <Row className="d-flex justify-content-center">
-//         <h1>Ride</h1>
-//       </Row>
-//     </Container>
-//   );
-// }
-
-// export default Ride;
