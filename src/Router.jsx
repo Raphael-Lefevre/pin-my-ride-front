a4ff0feac1f84db6e7a093b749312da36a10ae36
Raphael Lefevre <raph.lefevre@gmail.com>
2021-01-15 11:30:07 +0100

    2 rides on user page

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,6 +6,7 @@ import Login from './components/Login';
 import SignIn from './components/SignIn';
 import User from './components/User';
 import RideMap from './components/Ride';
+import Ride2Map from './components/Ride2';
 import CheckAuth from './CheckAuth';
 import Header from './components/Header';
 
@@ -29,8 +30,8 @@ const Router = () => {
         <Switch>
           <Route exact path="/login" component={Login} />
           <Route exact path="/signin" component={SignIn} />
+          <Route exact path="/ride2map" component={Ride2Map} />
           <Route exact path="/ridemap" component={RideMap} />
-
           <CheckAuth
             component={User}
             header={Header}
